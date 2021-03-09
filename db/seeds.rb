f8a665fa6059157e3d9fb9081c98f453c142a7c8
gjeffgolden <gjgolden08@gmail.com>
2021-03-09 15:24:38 -0700

    Re-seeded tasks.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 Event.destroy_all
 Task.destroy_all
 
-whiteboard = Task.create(
+Task.create(
+    name: "Lesson",
+    description: "Attend a live lecture or watch a pre-recorded video."
+)
+Task.create(
     name: "Whiteboard",
     description: "Solve code puzzles and algorithms on CodeWars, CodinGame, LeetCode, HackerRank or other similar platforms."
 )
 Task.create(
-    name: "Build and Burn",
-    description: "Practice new concepts through repitition."
+    name: "Code",
+    description: "Complete labs or assessments, study READMEs, build-and-burn basic apps to master new concepts through repetition, or work on a full-stack project." 
 )
 Task.create(
-    name: "Project",
-    description: "Focus on building a full-stack app for school or personal use."
+    name: "Water the Garden",
+    description: "Attend a social event, engage on LinkedIn, browse job listings, refresh your resume or otherwise enhance your personal brand."
 )
 Task.create(
-    name: "Network",
-    description: "Attend a social event, engage on LinkedIn, browse job listings, refresh your resume, enhance your personal brand, etc."
-)
-Task.create(
-    name: "Explore Tech",
+    name: "Explore",
     description: "Identify a new-to-you technology or concept and learn as much as you can about it within a preset time frame."
 )
 Task.create(
@@ -26,16 +26,6 @@ Task.create(
     description: "Write a blog post related to your personal tech journey, recently mastered concepts or your current projects."
 )
 Task.create(
-    name: "Labs and Assessments",
-    description: "Complete guided READMEs, labs and assessments."
-)
-Task.create(
     name: "Self-Care",
-    description: "Meditate, go for a walk, watch a TV show, workout, puzzle, play a video game: take a break from coding!"
-)
-
-Event.create(
-    task: whiteboard,
-    goal: 30,
-    notes: "Solve as many CodeWars katas as I can within 30 minutes."
+    description: "Take a break and refresh your brain! Meditate, go for a walk, watch a TV show, workout, puzzle, play a video game, etc."
 )
